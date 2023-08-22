e94c0c420e7e43bec0b45d780ab07caf443c568e
samirshrestha-tech <128810359+samirshrestha-tech@users.noreply.github.com>
2023-08-22 16:04:27 +1000

    javascript basic

---
+++ script.js
@@ -0,0 +1,134 @@
+// console.log("Hi JS developers");
+// single line comment
+
+/*
+multi line comment
+
+*/
+// alert("hello there, ");
+// we use alert mainly for early stage to debug the code
+
+// ====vairables===
+
+// var,let,const
+// var firstName; creating variables
+// var firstName = "Samir Shrestha"; //assigning value
+// let lastName = "fakfkafjk";
+// const dob = "20-20-2020";
+// console.log(firstName, lastName, dob);
+// firstName = "tim";
+// lastName = "smith";
+
+// console.log(firstName, lastName, dob);
+// ==== JS Arithmetic operators====
+//  Arithmetic
+// +	Addition
+// let a = 5;
+// const b = 10;
+// const result = a + b;
+// console.log(result);
+
+// -	Subtraction
+// const result = a - b;
+// console.log(result);
+// *	Multiplication
+// const result = a * b;
+// console.log(result);
+// console.log(result);
+// **	Exponentiation (ES2016)
+// const result = a ** b;
+// console.log(result);
+// /	Division
+// const result = a / b;
+// console.log(result);
+
+// console.log(result);
+// %	Modulus (Division Remainder)
+// const result = a % b;
+// console.log(result);
+
+// console.log(result);
+// ++	Increment
+// console.log(a);
+// console.log(a++);
+// console.log(++a);
+// a++;
+
+// console.log(result);
+// --	Decrement  */
+// console.log(a);
+// console.log(a--);
+// console.log(--a);
+// the increment or decrement operator when used infront or after first uses the value from the variable and the increases or decreases and vice-versa.
+
+// Comparisons
+
+// ==
+// ===
+// >, >=
+// <, <=
+
+// const a = 5;
+// const b = 5;
+// // const result = a > b;
+// // const result = a >= b;
+// // const result = a == b; equality operator
+// // const result = a === b;
+
+// console.log(result);
+// Data types
+// Primitives
+// const name = "samir";
+// console.log(typeof name, name);
+// String  		// “prem acharya”
+// Number 	//123
+// safe number - 2^53-1 to 2^53-1
+// const num = 3232_45874
+// const num = 4545;
+// console.log(num);
+
+// Boolen		// true or false
+// Undefined	// not defined in system memory
+// const val = undefined;
+
+// let val = null;
+// console.log(typeof val, val);
+// Null  		// defined in system memory but no value assigned
+// Reference Types
+// Object 		//{key:”value”}
+// const person = {
+//   firstName: "samir",
+//   lastName: "shrestha",
+// };
+// console.log(person);
+// console.log(person.firstName);
+// console.log(person["firstName"]);
+
+// Array		//[“data”, 1232, {}, []]
+
+// const arg = ["djsfslfj", 454, {}, [], true, null];
+// console.log(arg);
+// Function 	// function()
+// Es6 	// const sayHi ()=> “hi”
+
+// Math.PI;
+// const ans = Math.PI;
+// console.log(ans);
+// Math.round(4.7);
+// const ans = Math.round(4.6);
+// console.log(ans);
+// Math.pow(8, 2);
+// Math.sqrt(64);
+
+// Math.ceil(4.4);
+// const ans = Math.ceil(4.6);
+// console.log(ans);
+// Math.floor(4.7);
+// const ans = Math.floor(4.6);
+// console.log(ans);
+
+// Math.min(0, 150, 30, 20, -8, -200);
+// Math.max(0, 150, 30, 20, -8, -200);
+// Math.random();
+
+// console.log(ans);
